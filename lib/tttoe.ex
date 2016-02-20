fc7bdc37ec549868b8e5928f430c5151de649298
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-02-20 14:46:37 -0200

    Pequena alteração na função tttoe.

--- lib/tttoe.ex
+++ lib/tttoe.ex
@@ -43,7 +43,7 @@ defmodule Tttoe do
       and c7 != ' ' and c8 != ' ' and c9 != ' ' do
 
       draw({c1, c2, c3, c4, c5, c6, c7, c8, c9})
-      IO.puts("Velha!")
+      IO.puts("Tie game!")
 
   end
 
